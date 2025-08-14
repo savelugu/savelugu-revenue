6b0da0dc59dd95f63bac3572fb38949a5e7168e4
shazdataconsult <shazdataconsult@gmail.com>
2025-08-14 17:13:55 +0000

    Configure DATABASE_URL and fix deployment settings

--- revenue_mobilization/settings.py
+++ revenue_mobilization/settings.py
@@ -42,11 +42,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 #ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
-
+from decouple import config
 import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 
 
@@ -106,12 +106,12 @@ LOGOUT_REDIRECT_URL = 'login'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+#DATABASES = {
+    # "default": {
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+    # }
+# }
 
 
 # Password validation
