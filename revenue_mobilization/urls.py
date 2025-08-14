1459c120f10616c8fd8a9a294cfbe169a6f5a150
shazdataconsult <shazdataconsult@gmail.com>
2025-08-14 17:32:50 +0000

    Add migration trigger route

--- revenue_mobilization/urls.py
+++ revenue_mobilization/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from core.views import run_migrations
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('run-migrations/', run_migrations), 
     path('', include('core.urls')),  # Core app URLs
     
 ]
