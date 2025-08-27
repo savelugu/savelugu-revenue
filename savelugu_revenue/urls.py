5065678fff1bc2daa37ed24b107471816fa4358b
shazdataconsult <shazdataconsult@gmail.com>
2025-08-27 14:33:11 +0000

    Set root URL to home page and added home.html

--- savelugu_revenue/urls.py
+++ savelugu_revenue/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from core.views import run_migrations
+from core import views
+
+
 
 urlpatterns = [
+    path('', views.home, name='home'),   # 👈 Now root goes to home page
     path('admin/', admin.site.urls),
     path('run-migrations/', run_migrations), 
     path('', include('core.urls')),  # Core app URLs
